schleiereule/ontopia

commit fbcc875a14259a569a5901106328f4fbc05778f4

Automated removal of new Boolean()

ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/VizConfigurationManager.java
@@ -196,7 +196,7 @@ public abstract class VizConfigurationManager {
    * map.
    */
   protected void setOccurenceValue(TopicIF type, TopicIF occtype, boolean value) {
-    setOccurenceValue(type, occtype, (new Boolean(value)).toString());
+    setOccurenceValue(type, occtype, (value).toString());
   }
 
   public String getOccurrenceValue(TopicIF type, TopicIF occtype) {

ontopia-engine/src/main/java/net/ontopia/topicmaps/entry/XMLConfigSource.java
@@ -342,7 +342,7 @@ public class XMLConfigSource {
                 break;
               }
               else if (props[i].getPropertyType().equals(boolean.class)) {
-                setter.invoke(source, new Object[] {new Boolean(PropertyUtils.isTrue(param_value))});
+                setter.invoke(source, new Object[] {PropertyUtils.isTrue(param_value)});
                 found_property = true;
                 break;
               }

ontopia-webed/src/main/java/net/ontopia/topicmaps/webed/taglibs/form/ButtonTag.java
@@ -95,7 +95,7 @@ public class ButtonTag extends TagSupport implements ActionInvokingTagIF {
     }
 
     if (id != null) vc.put("id", id);
-    vc.put("readonly", new Boolean(readonly));
+    vc.put("readonly", readonly);
     if (klass != null) vc.put("class", klass);
 
     sub_actions = new ArrayList(); // we've used these now, can't retain them

ontopia-webed/src/main/java/net/ontopia/topicmaps/webed/taglibs/form/CheckboxTag.java
@@ -107,7 +107,7 @@ public class CheckboxTag extends TagSupport implements ActionInvokingTagIF {
       vc.put("checked", "");      
 
     if (id != null) vc.put("id", id);
-    vc.put("readonly", new Boolean(readonly));
+    vc.put("readonly", readonly);
     if (klass != null) vc.put("class", klass);
     
     sub_actions = new ArrayList(); // we've used these now, can't retain them

ontopia-webed/src/main/java/net/ontopia/topicmaps/webed/taglibs/form/FieldTag.java
@@ -118,7 +118,7 @@ public class FieldTag extends BodyTagSupport {
     }
 
     // fill in attribute values
-    vc.put("readonly", new Boolean(readonly));
+    vc.put("readonly", readonly);
     vc.put("value", value);
 
     if (id != null) vc.put("id", id);

ontopia-webed/src/main/java/net/ontopia/topicmaps/webed/taglibs/form/FileTag.java
@@ -86,7 +86,7 @@ public class FileTag extends BodyTagSupport {
       vc.put("name", name);
     }
     
-    vc.put("readonly", new Boolean(readonly));
+    vc.put("readonly", readonly);
     vc.put("value", value);
 
     if (id != null) vc.put("id", id);

ontopia-webed/src/main/java/net/ontopia/topicmaps/webed/taglibs/form/FormTag.java
@@ -140,7 +140,7 @@ public class FormTag extends BodyTagSupport {
 
     boolean readonly =
       InteractionELSupport.getBooleanValue(this.readonly, false, pageContext);
-    request.setAttribute(Constants.OKS_FORM_READONLY, new Boolean(readonly));
+    request.setAttribute(Constants.OKS_FORM_READONLY, readonly);
 
     // put the name of the action group to page scope
     // to allow child tags to access this information
@@ -280,7 +280,7 @@ public class FormTag extends BodyTagSupport {
     // FIXME: Do we really need this line? Probably not, since each control
     // should now itself be responisible for determining whether it should be
     // readonly. Hence the individual control can overrule the form setting.
-    // vc.put("readonly", new Boolean(TagUtils.isFormReadOnly(request)));
+    // vc.put("readonly", TagUtils.isFormReadOnly(request));
 
     // content inside the form element
     BodyContent body = getBodyContent();

ontopia-webed/src/main/java/net/ontopia/topicmaps/webed/taglibs/form/LinkTag.java
@@ -106,7 +106,7 @@ public class LinkTag extends BodyTagSupport {
     }
 
     // read-only state
-    vc.put("readonly", new Boolean(readonly));
+    vc.put("readonly", readonly);
         
     if (href != null)
       vc.put("href", href);

commit 30eaba4ce74825c47c72ffa4d5b2609a4de892e5

Fixed compile error caused by fbcc875a14259a569a5901106328f4fbc05778f4

ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/VizConfigurationManager.java
@@ -196,7 +196,7 @@ public abstract class VizConfigurationManager {
    * map.
    */
   protected void setOccurenceValue(TopicIF type, TopicIF occtype, boolean value) {
-    setOccurenceValue(type, occtype, (value).toString());
+    setOccurenceValue(type, occtype, Boolean.toString(value));
   }
 
   public String getOccurrenceValue(TopicIF type, TopicIF occtype) {