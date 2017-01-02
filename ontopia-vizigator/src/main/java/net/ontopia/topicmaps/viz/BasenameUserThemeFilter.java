schleiereule/ontopia

commit fc05f95f6bcf57992f31b8c0862ac65ed58db315

Added missing method and field modifiers to avoid use of package-private access

ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/BasenameUserThemeFilter.java
@@ -40,7 +40,7 @@ import org.slf4j.LoggerFactory;
 public class BasenameUserThemeFilter extends AbstractUserThemeFilter {
 
   // initialize logging category
-  static Logger log = LoggerFactory.getLogger(BasenameUserThemeFilter.class.getName());
+  private static Logger log = LoggerFactory.getLogger(BasenameUserThemeFilter.class.getName());
 
   // members
   protected ScopeIndexIF scopeIndex;

ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/ExtendedTGPanel.java
@@ -157,16 +157,16 @@ public class ExtendedTGPanel extends TGPanel {
 
   protected Node select;
 
-  Node dragNode; // Node currently being dragged
+  private Node dragNode; // Node currently being dragged
 
   protected Point mousePos; // Mouse location, updated in the
                             // mouseMotionListener
 
-  Image offscreen;
+  private Image offscreen;
 
-  Dimension offscreensize;
+  private Dimension offscreensize;
 
-  Graphics offgraphics;
+  private Graphics offgraphics;
 
   private Vector graphListeners;
 
@@ -174,11 +174,11 @@ public class ExtendedTGPanel extends TGPanel {
 
   // Converts between a nodes visual position (drawx, drawy), and its absolute
   // position (x,y).
-  TGLensSet tgLensSet;
+  private TGLensSet tgLensSet;
 
-  TGPanel.AdjustOriginLens adjustOriginLens;
+  private TGPanel.AdjustOriginLens adjustOriginLens;
 
-  TGPanel.SwitchSelectUI switchSelectUI;
+  private TGPanel.SwitchSelectUI switchSelectUI;
 
   public Image image;
 
@@ -513,7 +513,7 @@ public class ExtendedTGPanel extends TGPanel {
     }
   }
 
-  void fireMovedEvent() {
+  private void fireMovedEvent() {
     Vector listeners;
 
     // The following condition was added because the synchronized line below
@@ -674,7 +674,7 @@ public class ExtendedTGPanel extends TGPanel {
     return dragNode;
   }
 
-  void setMousePos(Point p) {
+  private void setMousePos(Point p) {
     mousePos = p;
   }
 
@@ -816,9 +816,9 @@ public class ExtendedTGPanel extends TGPanel {
       setMouseOverE(null);
   }
 
-  TGPoint2D topLeftDraw = null;
+  private TGPoint2D topLeftDraw = null;
 
-  TGPoint2D bottomRightDraw = null;
+  private TGPoint2D bottomRightDraw = null;
 
   public TGPoint2D getTopLeftDraw() {
     return new TGPoint2D(topLeftDraw);
@@ -898,7 +898,7 @@ public class ExtendedTGPanel extends TGPanel {
     visibleLocality.forAllNodes(fen);
   }
 
-  Color myBrighter(Color c) {
+  private Color myBrighter(Color c) {
     int r = c.getRed();
     int g = c.getGreen();
     int b = c.getBlue();

ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/MenuFileParser.java
@@ -45,9 +45,9 @@ import net.ontopia.utils.StreamUtils;
  */
 public class MenuFileParser {
   private String source;
-  Map enabled;
-  int lineIndex;
-  boolean succeeded;
+  private Map enabled;
+  private int lineIndex;
+  private boolean succeeded;
   
   /**
    * Create new from a given file.
@@ -242,7 +242,7 @@ public class MenuFileParser {
    * Indicates a parse or syntax error in MenuFileParser.
    */
   public class MenuFileParseError extends Exception {
-    String message;
+    private String message;
     
     public MenuFileParseError(String message) {
       this.message = message;

ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/MotionKiller.java
@@ -59,8 +59,8 @@ public class MotionKiller extends TimerTask {
   protected int maxCycle = 3;
   protected boolean enabled;
   
-  long millis;
-  Timer timer;  
+  private long millis;
+  private Timer timer;  
 
   /**
    * Create a MotionKiller for the given tgPanel, scheduled to run every

ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/ProbabilityManager.java
@@ -39,8 +39,8 @@ import net.ontopia.utils.OntopiaRuntimeException;
  */
 public class ProbabilityManager {
   private Map probabilities;
-  Random random;
-  boolean keysInOrder;
+  private Random random;
+  private boolean keysInOrder;
 
   public ProbabilityManager(Object key) {
     probabilities = new TreeMap();

ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/AbstractUserThemeFilter.java
@@ -33,7 +33,7 @@ import net.ontopia.topicmaps.core.TopicMapIF;
  */
 public abstract class AbstractUserThemeFilter {
 
-  TopicMapIF topicMap;
+  protected TopicMapIF topicMap;
 
   public AbstractUserThemeFilter() {
     this.topicMap = null;

ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/AssociationScopeFilterMenu.java
@@ -153,8 +153,8 @@ public class AssociationScopeFilterMenu extends JMenu {
    */
   protected class ScopeFilterActionListener implements ActionListener {
     private TopicIF scope;
-    ActionListener parentListener;
-    VizController controller;
+    private ActionListener parentListener;
+    private VizController controller;
 
     /**
      * Create new.
@@ -215,9 +215,9 @@ public class AssociationScopeFilterMenu extends JMenu {
    * Listens for changes to the scrictness of the scope filter.
    */
   protected class StrictnessActionListener implements ActionListener {
-    VizController controller;
-    int strictnessLevel;
-    ActionListener parentListener;
+    private VizController controller;
+    private int strictnessLevel;
+    private ActionListener parentListener;
     
     /**
      * 

ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/OSpinner.java
@@ -46,8 +46,8 @@ public class OSpinner extends JPanel {
   private int increment = 1;
   private int value = min;
 
-  JButton north;
-  JButton south;
+  private JButton north;
+  private JButton south;
   
   private Box buttonPanel = new Box(BoxLayout.Y_AXIS);
   private NumberFormat formatter = NumberFormat.getNumberInstance();

commit ca3acfb19477666f296e2f2b238e595314ab92c1

Improved code that caused new issues in fc05f95f6bcf57992f31b8c0862ac65ed58db315

ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/BasenameUserThemeFilter.java
@@ -30,17 +30,11 @@ import net.ontopia.topicmaps.core.index.ClassInstanceIndexIF;
 import net.ontopia.topicmaps.core.index.ScopeIndexIF;
 import net.ontopia.topicmaps.utils.TypeHierarchyUtils;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 /**
  * INTERNAL: Abstract class which provides access to filtering themes
  * out which are not relevant to the user context.
  */
 public class BasenameUserThemeFilter extends AbstractUserThemeFilter {
-
-  // initialize logging category
-  private static Logger log = LoggerFactory.getLogger(BasenameUserThemeFilter.class.getName());
 
   // members
   protected ScopeIndexIF scopeIndex;

ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/ExtendedTGPanel.java
@@ -182,6 +182,10 @@ public class ExtendedTGPanel extends TGPanel {
 
   public Image image;
 
+  private TGPoint2D topLeftDraw = null;
+
+  private TGPoint2D bottomRightDraw = null;
+
   // ............
 
   /**
@@ -674,10 +678,6 @@ public class ExtendedTGPanel extends TGPanel {
     return dragNode;
   }
 
-  private void setMousePos(Point p) {
-    mousePos = p;
-  }
-
   public Point getMousePos() {
     return mousePos;
   }
@@ -816,10 +816,6 @@ public class ExtendedTGPanel extends TGPanel {
       setMouseOverE(null);
   }
 
-  private TGPoint2D topLeftDraw = null;
-
-  private TGPoint2D bottomRightDraw = null;
-
   public TGPoint2D getTopLeftDraw() {
     return new TGPoint2D(topLeftDraw);
   }
@@ -896,18 +892,6 @@ public class ExtendedTGPanel extends TGPanel {
       }
     };
     visibleLocality.forAllNodes(fen);
-  }
-
-  private Color myBrighter(Color c) {
-    int r = c.getRed();
-    int g = c.getGreen();
-    int b = c.getBlue();
-
-    r = Math.min(r + 96, 255);
-    g = Math.min(g + 96, 255);
-    b = Math.min(b + 96, 255);
-
-    return new Color(r, g, b);
   }
 
   public synchronized void paint(Graphics g) {

ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/MenuFileParser.java
@@ -47,7 +47,6 @@ public class MenuFileParser {
   private String source;
   private Map enabled;
   private int lineIndex;
-  private boolean succeeded;
   
   /**
    * Create new from a given file.

ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/MotionKiller.java
@@ -59,9 +59,6 @@ public class MotionKiller extends TimerTask {
   protected int maxCycle = 3;
   protected boolean enabled;
   
-  private long millis;
-  private Timer timer;  
-
   /**
    * Create a MotionKiller for the given tgPanel, scheduled to run every
    * 'millis' number of milliseconds after creation.
@@ -72,8 +69,7 @@ public class MotionKiller extends TimerTask {
     this.tgPanel = tgPanel;
     waitUntil1 = 0;
     waitUntil2 = 0;
-    this.millis = millis;
-    timer = new Timer();
+    Timer timer = new Timer();
     timer.scheduleAtFixedRate(this, millis, millis);
     
     // Note: VizPanel assumes this assignment when building menus.

ontopia-vizigator/src/main/java/net/ontopia/topicmaps/viz/ProbabilityManager.java
@@ -40,13 +40,11 @@ import net.ontopia.utils.OntopiaRuntimeException;
 public class ProbabilityManager {
   private Map probabilities;
   private Random random;
-  private boolean keysInOrder;
 
   public ProbabilityManager(Object key) {
     probabilities = new TreeMap();
     probabilities.put(key, new Double(1));
     random = new Random();
-    keysInOrder = false;
   }
   
   /**